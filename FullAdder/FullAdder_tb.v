12b88c4937ebfb1087f450073a47d35d79d22813
unknown <olugbonfemi@gmail.com>
2022-01-12 20:13:08 -0500

    made changes to the testbench

--- FullAdder/FullAdder_tb.v
+++ FullAdder/FullAdder_tb.v
@@ -3,69 +3,6 @@ module FullAdder_tb();
     wire Sum, Cout;
 
     FullAdder simulate_adder (
-        A,
-        B,
-        Cin,
-        Sum, 
-        Cout
-    );
-
-    initial
-        begin
-            $display($time, " <<Starting the Simulation >>");
-            // We will loop through all possible input combinations with 50 nano second delay in between each combination
-
-            A = 1'b0; //represent logic 0 (LOW)
-            B = 1'b0;
-            Cin = 1'b0; // How binary bit are represented in verilog
-            #50
-
-            A = 1'b0;
-            B = 1'b0;
-            Cin = 1'b1;  //represent logic 1 (HIGH)
-            #50
-
-            A = 1'b0;
-            B = 1'b1;
-            Cin = 1'b0;
-            #50
-
-            A = 1'b0;
-            B = 1'b1;
-            Cin = 1'b1;
-            #50
-
-            A = 1'b1;
-            B = 1'b0;
-            Cin = 1'b0;
-            #50
-
-            A = 1'b1;
-            B = 1'b0;
-            Cin = 1'b1;
-            #50
-
-            A = 1'b1;
-            B = 1'b1;
-            Cin = 1'b0;
-            #50
-
-            A = 1'b1;
-            B = 1'b1;
-            Cin = 1'b1;
-            #50
-            $display($time, " << Simulation complete>>");
-        end
-endmodule
-
-
-
-//new code
-module FullAdder_tb();
-    reg A, B, Cin;
-    wire Sum, Cout;
-
-    FullAdder simulate_adder (
         .A(A),
         .B(B),
         .Cin(Cin),
